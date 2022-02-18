a7fab6423861b506cf8c8c75e25ec77c03109a59
rizalzaenal <rizaenalofficial@gmail.com>
2022-02-18 10:37:22 +0700

    init project

---
+++ app/src/main/java/com/example/githubapp/ui/search_user/SearchUserFragment.kt
@@ -0,0 +1,17 @@
+package com.example.githubapp.ui.search_user
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.githubapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchUserFragment: Fragment(R.layout.fragment_search_user) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
